61dc3d9872432d2a7b4535a6a1167126f9e5b271
Juan Rios <Juancarlosrioscolorado@gmail.com>
2026-05-02 15:22:32 -0500

    Creación de los servicios para los services de la barberia. Ajuste del contexto de servicios

--- BarberiaApp/context/ServiceContext.tsx
+++ BarberiaApp/context/ServiceContext.tsx
@@ -1,27 +1,97 @@
-// import { Servicio } from "@/models/Servicio";
-// import { createContext, ReactNode } from "react";
+import { Servicio } from "@/models/Servicio";
+import { createService, deleteService, getServices, updateService } from "@/services/service-services";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 
 
-// interface ServiceContextType {
-//     servicios: Servicio[];
-//     addServices: (service: Omit<Servicio, 'id'>) => Promise<void>;
-//     updateServices: (id: string, updatedService: Partial<Servicio>) => Promise<void>;
-//     deleteServices: (id: string) => Promise<void>;
-// }
+interface ServiceContextType {
+    servicios: Servicio[];
+    loading: boolean;
+    refreshServices: () => Promise<void>;
+    addServices: (service: Omit<Servicio, 'id'>) => Promise<void>;
+    updateServices: (id: string, updatedService: Partial<Servicio>) => Promise<void>;
+    deleteServices: (id: string) => Promise<void>;
+}
 
-// const ServiceContext = createContext<ServiceContextType | undefined>(undefined);
+const ServiceContext = createContext<ServiceContextType | undefined>(undefined);
 
-// export const ServiceProvider: ({ children}: {children: ReactNode}) => {
+export const ServiceProvider = ({ children }: { children: ReactNode }) => {
 
-//     const [servicios, setServicios] = useState<Servicio[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [servicios, setServicios] = useState<Servicio[]>([]);
 
-//     const addServices = async (new: Omit<Servicio, 'id'>) => {
-//         try {
-//             const response = await createService (new as Servicio);
-//             setServicios()
-            
-//         }
-//     }
+    useEffect(() => {
+        refreshServices();
+    }, [])
 
 
-// }
+    const refreshServices = async () => {
+        try {
+            setLoading(true);
+            const data = await getServices();
+            setServicios(data || []);
+
+        } catch (error) {
+            console.error('Error al cargar los servicios:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const addServices = async (newServices: Omit<Servicio, 'id'>) => {
+        try {
+            const response = await createService(newServices as Servicio);
+            setServicios(prev => [...prev, response]);
+        } catch (error) {
+            console.error('Error al agregar el servicio:', error);
+        }
+    };
+
+    const updateServices = async (id: string, updatedService: Partial<Servicio>) => {
+        try {
+            const response = await updateService(id, updatedService);
+            setServicios(prev => prev.map((s) => s.id === id ? response : s));
+        } catch (error) {
+            console.error('Error al actualizar el servicio:', error);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+    const deleteServices = async (id: string) => {
+        try {
+            setLoading(true);
+            await deleteService(id);
+            setServicios((prev) => prev.filter((s) => s.id !== id));
+        } catch (error) {
+            console.error('Error al eliminar el servicio:', error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <ServiceContext.Provider
+            value={{
+                servicios,
+                loading,
+                refreshServices,
+                addServices,
+                updateServices,
+                deleteServices
+            }}
+        >
+            {children}
+        </ServiceContext.Provider>
+    )
+}
+
+
+export const useService = () => {
+    const context = useContext(ServiceContext);
+    if (!context) {
+        throw new Error('useService debe ser utilizado dentro de un ServiceProvider');
+    }
+    return context;
+}
+
